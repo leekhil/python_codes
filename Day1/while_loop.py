d825194a88477981bd32564fabb1c3fbb56a4c51
leekhil <leekhillikku99@gmail.com>
2022-08-11 09:33:47 +0530

    Day1 codes

---
+++ Day1/while_loop.py
@@ -0,0 +1,16 @@
+#print the table
+i=1
+while i <= 10:
+	print(i*2 )
+	i = i+1 #i+=1
+
+print("By for loop")
+for i in range(1,11):
+	print(i*2)
+
+
+print("Even number till 20")
+for x in range(1,21):
+	if(x%2 == 0):
+		print(x,"is even")
+
